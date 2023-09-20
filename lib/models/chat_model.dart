c99cbb08487f99442968ddccec8c4d6f74b4580b
Sandeep Adithya K <sandeepadithya@outlook.com>
2023-09-20 12:21:30 +0530

    Clg Cmit

--- lib/models/chat_model.dart
+++ lib/models/chat_model.dart
@@ -12,11 +12,11 @@ class Message {
 }
 
 List<Message> messages = [
-  Message(id: 0, received: false, text: " Hi ", timeStamp: '4:30 PM'),
-  Message(id: 1, received: true, text: "Yes, same h", timeStamp: '4:31 PM'),
-  Message(id: 2, received: false, text: "Thank you. .", timeStamp: '4:32 PM'),
+  Message(id: 0, received: false, text: " Hi Iam Vijay Bala", timeStamp: '4:30 PM'),
+  Message(id: 1, received: true, text: "Yes, Iam Vijay Bala Too", timeStamp: '4:31 PM'),
+  Message(id: 2, received: false, text: "Ooo Nice to meet you.", timeStamp: '4:32 PM'),
   Message(id: 3, received: true, text: "Thanks, ", timeStamp: '4:33 PM'),
   Message(id: 4, received: false, text: "I wanted", timeStamp: '4:33 PM'),
   Message(id: 5, received: true, text: " I agree", timeStamp: '4:33 PM'),
-  Message(id: 6, received: false, text: "Sure. I think", timeStamp: '4:33 PM'),
+  Message(id: 6, received: false, text: "Sure. I think", timeStamp: '4:34 PM'),
 ];
