06adb1a7e8b525a7f8f23b7b37d6dd6cdaba2ef9
Mukunth <jmukunth55@gmail.com>
2023-09-20 20:13:24 +0530

    merging

---
+++ lib/utils/app_styles.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+const Color cPrimaryBlue = Color(0xff3369ff);
+const Color cWhite = Color(0xFFFFFFFF);
+const Color cGrey = Color(0xFFD8D8D8);
+const Color cSenderBubbleCar = Color(0xFFEEEEEE);
+
+const Color cBlack = Color(0xff3f3f3f);
+const Color cGreen = Color(0xff3abf38);
+
+final cRegular = GoogleFonts.nunito(
+  fontWeight: FontWeight.w500,
+);
+
+final cSemiBold = GoogleFonts.nunito(
+  fontWeight: FontWeight.w600,
+);
+
+final cBold = GoogleFonts.nunito(
+  fontWeight: FontWeight.w800,
+);
